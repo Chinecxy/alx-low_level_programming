283f42b28f7526f34655d798faead5d29f09d2bb
Chinecxy <natochinecxy008@gmail.com>
2022-09-12 16:51:36 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,9 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
